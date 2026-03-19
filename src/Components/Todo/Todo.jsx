7ef3df66f6721cfd294944b33db90f9c65308013
MaksumulEmon <maksumulemon300@gmail.com>
2026-03-20 00:52:20 +0600

    Conditional Border Aded

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+// import React, { useState } from 'react';
 
 const Todo = ({ todo }) => {
+    // const [todos, setTodos] = useState([]);
     return (
-        <div className='border-4 border-green-600  rounded-xl p-4 m-4 md:m-0'>
+        <div className={`border-4 border-green-400  rounded-xl p-4 m-4 md:m-0  shadow-2xl ${todo.completed? "border-freen-400" :"border-red-400"}`}>
             <h2 className='text-lg font-bold '>Name:{todo.todo}</h2>
             <p>{todo.completed ? "✅ Done" : "❌ Not Done"}</p>
-            <button>Ststus</button>
+            {/* <button className='btn btn-success mt-5'>Ststus</button> */}
         </div>
     );
 };
 
 export default Todo;
+
+
+
--- src/Components/Todos/Todos.jsx
+++ src/Components/Todos/Todos.jsx
@@ -18,3 +18,5 @@ const Todos = ({todospromise}) => {
 };
 
 export default Todos;
+
+
